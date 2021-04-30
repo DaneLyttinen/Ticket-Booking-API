d0cff870b8f43f19978c54edc4e45c55a338ca28
rmcl772 <69742096+rmcl772@users.noreply.github.com>
2021-05-01 01:41:01 +1200

    Added performer routes to concertResource

--- concert-service/src/main/java/asg/concert/service/services/ConcertResource.java
+++ concert-service/src/main/java/asg/concert/service/services/ConcertResource.java
@@ -2,8 +2,10 @@ package asg.concert.service.services;
 
 import asg.concert.common.dto.ConcertDTO;
 import asg.concert.common.dto.ConcertSummaryDTO;
+import asg.concert.common.dto.PerformerDTO;
 import asg.concert.service.common.Config;
 import asg.concert.service.domain.Concert;
+import asg.concert.service.domain.Performer;
 import asg.concert.service.mapper.Mapper;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -24,16 +26,14 @@ import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
-@Path("/concert-service/concerts")
+@Path("/concert-service")
 public class ConcertResource {
 
     private static Logger LOGGER = LoggerFactory.getLogger(ConcertResource.class);
 
-
-
     // TODO Implement this.
     @GET
-    @Path("{id}")
+    @Path("concerts/{id}")
     @Produces(javax.ws.rs.core.MediaType.APPLICATION_JSON)
     public Response retrieveConcert(@PathParam("id") long id) {
         LOGGER.info("Retrieving concert with id: " + id);
@@ -41,14 +41,13 @@ public class ConcertResource {
         Concert concert = null;
         ConcertDTO concertDTO = null;
         try {
-
             // Start a new transaction.
             em.getTransaction().begin();
-            concert = em.find(Concert.class, id);
+            
             // Use the EntityManager to retrieve, persist or delete object(s).
             // Use em.find(), em.persist(), em.merge(), etc...
-            concertDTO = Mapper.concertToDTO(concert);
-            //concertDTO = mapper.convertTo(concert);
+            concert = em.find(Concert.class, id);
+
             // Commit the transaction.
             em.getTransaction().commit();
         } finally {
@@ -58,6 +57,10 @@ public class ConcertResource {
         if (concert == null){
             throw new WebApplicationException(Response.Status.NOT_FOUND);
         }
+        
+        // Convert to DTO after confirming concert is not null
+        concertDTO = Mapper.concertToDTO(concert);
+
         return Response
                 .status(200)
                 .entity(concertDTO)
@@ -65,37 +68,38 @@ public class ConcertResource {
     }
 
     @GET
+    @Path("concerts")
     @Produces(javax.ws.rs.core.MediaType.APPLICATION_JSON)
     public Response getAllConcerts(){
         LOGGER.info("Retrieving all concerts: " );
         EntityManager em = PersistenceManager.instance().createEntityManager();
-        List<ConcertDTO> concertsDTO = new ArrayList<>();
+        List<ConcertDTO> concertDTOs = new ArrayList<>();
         try {
 
             // Start a new transaction.
             em.getTransaction().begin();
             TypedQuery<Concert> concertQuery = em.createQuery("select c from Concert c", Concert.class);
             List<Concert> concerts = concertQuery.getResultList();
-
+            
             for (Concert concert : concerts){
-                concertsDTO.add(Mapper.concertToDTO(concert));
+                concertDTOs.add(Mapper.concertToDTO(concert));
             }
 
-            //concertDTO = mapper.convertTo(concert);
             // Commit the transaction.
             em.getTransaction().commit();
         } finally {
             // When you're done using the EntityManager, close it to free up resources.
             em.close();
         }
+
         return Response
                 .status(200)
-                .entity(concertsDTO)
+                .entity(concertDTOs)
                 .build();
     }
 
     @GET
-    @Path("summaries")
+    @Path("concerts/summaries")
     @Produces(javax.ws.rs.core.MediaType.APPLICATION_JSON)
     public Response getConcertSummaires() {
         LOGGER.info("Retrieving all concert summaries: " );
@@ -125,6 +129,72 @@ public class ConcertResource {
         return Response
                 .status(200)
                 .entity(concertSummariesDTO)
+                .build();
+    }
+
+    @GET
+    @Path("performers/{id}")
+    @Produces(javax.ws.rs.core.MediaType.APPLICATION_JSON)
+    public Response getPerformer(@PathParam("id") long id) {
+        LOGGER.info("Retrieving performer with id: " + id);
+        EntityManager em = PersistenceManager.instance().createEntityManager();
+        Performer performer = null;
+        PerformerDTO performerDTO = null;
+        try {
+
+            // Start a new transaction.
+            em.getTransaction().begin();
+            
+            // Use the EntityManager to retrieve, persist or delete object(s).
+            // Use em.find(), em.persist(), em.merge(), etc...
+            performer = em.find(Performer.class, id);
+            
+            // Commit the transaction.
+            em.getTransaction().commit();
+        } finally {
+            // When you're done using the EntityManager, close it to free up resources.
+            em.close();
+        }
+        if (performer == null){
+            return Response.status(404).build();
+        }
+        
+        // Convert to DTO after confirming performer is not null
+        performerDTO = Mapper.performerToDTO(performer);
+
+        return Response
+                .status(200)
+                .entity(performerDTO)
+                .build();
+    }
+
+    @GET
+    @Path("performers")
+    @Produces(javax.ws.rs.core.MediaType.APPLICATION_JSON)
+    public Response getAllPerformers(){
+        LOGGER.info("Retrieving all performers: " );
+        EntityManager em = PersistenceManager.instance().createEntityManager();
+        List<PerformerDTO> performerDTOs = new ArrayList<>();
+        try {
+
+            // Start a new transaction.
+            em.getTransaction().begin();
+            TypedQuery<Performer> performerQuery = em.createQuery("select p from Performer p", Performer.class);
+            List<Performer> performers = performerQuery.getResultList();
+            
+            for (Performer performer : performers){
+                performerDTOs.add(Mapper.performerToDTO(performer));
+            }
+
+            // Commit the transaction.
+            em.getTransaction().commit();
+        } finally {
+            // When you're done using the EntityManager, close it to free up resources.
+            em.close();
+        }
+        return Response
+                .status(200)
+                .entity(performerDTOs)
                 .build();
     }
 }
