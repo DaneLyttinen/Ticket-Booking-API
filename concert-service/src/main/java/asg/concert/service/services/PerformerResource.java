e1e382471d4cea5b1c817cf1261da3f5a0b52549
Nathan Longhurst <nlon982@aucklanduni.ac.nz>
2021-05-09 14:44:54 +1200

    Making the code for ConcertResource and PerformerResource clean. Namely: adding documentation to each method (albeit, is repetitive), using Response.Status enum instead of numbers, and having consistency between the code

--- concert-service/src/main/java/asg/concert/service/services/PerformerResource.java
+++ concert-service/src/main/java/asg/concert/service/services/PerformerResource.java
@@ -22,68 +22,78 @@ public class PerformerResource {
 
     private static Logger LOGGER = LoggerFactory.getLogger(PerformerResource.class);
 
+    /**
+     * Retrieves a performer with a specific id, and return to the
+     * user in the form of a PerformerDTO. You don't have to be
+     * authenticated (and hence don't have to be authorised) to do
+     * this.
+     * 
+     * Throws Response.Status.NOT_FOUND if a performer doesn't
+     * exist with that id.
+     */
     @GET
     @Path("{id}")
     @Produces(javax.ws.rs.core.MediaType.APPLICATION_JSON)
     public Response getPerformer(@PathParam("id") long id) {
         LOGGER.info("Retrieving performer with id: " + id);
-        EntityManager em = PersistenceManager.instance().createEntityManager();
-        Performer performer = null;
-        PerformerDTO performerDTO = null;
-        try {
 
-            // Start a new transaction.
+        EntityManager em = PersistenceManager.instance().createEntityManager();
+
+        Performer performer = null;
+
+        try {
             em.getTransaction().begin();
 
-            // Use the EntityManager to retrieve, persist or delete object(s).
-            // Use em.find(), em.persist(), em.merge(), etc...
             performer = em.find(Performer.class, id);
 
-            // Commit the transaction.
             em.getTransaction().commit();
         } finally {
-            // When you're done using the EntityManager, close it to free up resources.
             em.close();
         }
         if (performer == null){
-            return Response.status(404).build();
+            return Response.status(Response.Status.NOT_FOUND).build();
         }
 
         // Convert to DTO after confirming performer is not null
-        performerDTO = Mapper.convertObj(performer,  new TypeReference<PerformerDTO>(){});
+        PerformerDTO performerDTO = Mapper.convertObj(performer,  new TypeReference<PerformerDTO>(){});
 
-        return Response
-                .status(200)
-                .entity(performerDTO)
-                .build();
+        return Response.status(Response.Status.OK).entity(performerDTO).build();
     }
 
+    /**
+     * Retrieves all performers, and returns to the client in the 
+     * form of a list of PerformerDTOs. You don't have to be authenticated 
+     * (and hence don't have to be authorised) to do this.
+     * 
+     * There's no possibility error that can happen, i.e., if no performers
+     * exist, this just returns a empty list. So no error can be thrown by
+     * this method. (Unless there's a mistake with the code)
+     */
     @GET
     @Produces(javax.ws.rs.core.MediaType.APPLICATION_JSON)
     public Response getAllPerformers(){
-        LOGGER.info("Retrieving all performers: " );
-        EntityManager em = PersistenceManager.instance().createEntityManager();
-        List<PerformerDTO> performerDTOs = new ArrayList<>();
-        try {
+        LOGGER.info("Retrieving all performers: ");
 
-            // Start a new transaction.
+        EntityManager em = PersistenceManager.instance().createEntityManager();
+
+        List<PerformerDTO> performerDTOs = new ArrayList<>(); // having this here means that if the following doesn't retrieve any performers, an empty list is returned no problem
+
+        try {
             em.getTransaction().begin();
+
             TypedQuery<Performer> performerQuery = em.createQuery("select p from Performer p", Performer.class);
             List<Performer> performers = performerQuery.getResultList();
 
+            // Convert to list of performer DTOs
             for (Performer performer : performers){
                 performerDTOs.add(Mapper.convertObj(performer,  new TypeReference<PerformerDTO>(){}));
             }
 
-            // Commit the transaction.
             em.getTransaction().commit();
         } finally {
-            // When you're done using the EntityManager, close it to free up resources.
             em.close();
         }
-        return Response
-                .status(200)
-                .entity(performerDTOs)
-                .build();
+
+        return Response.status(Response.Status.OK).entity(performerDTOs).build();
     }
 }
