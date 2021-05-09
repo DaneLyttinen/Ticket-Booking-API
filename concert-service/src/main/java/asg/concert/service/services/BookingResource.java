e28cd93e3a2bf4d8bdbd55b7827bbe2a5a6de419
Dane Lyttinen <69489249+DaneLyttinen@users.noreply.github.com>
2021-05-09 22:49:10 +1200

    Made it work correctly

--- concert-service/src/main/java/asg/concert/service/services/BookingResource.java
+++ concert-service/src/main/java/asg/concert/service/services/BookingResource.java
@@ -118,17 +118,17 @@ public class BookingResource {
                 LOGGER.info(String.format("There does not exist a concert with the id '%d'. Throwing Response.Status.BAD_REQUEST", desiredConcertIdToBook));
                 throw new WebApplicationException(Response.Status.BAD_REQUEST);
             }
-
+            em.getTransaction().commit();
             if (! desiredConcertToBook.getDates().contains(desiredDateToBook)) {
                 LOGGER.info("The concert that the client is intending to book seats for is NOT scheduled on this date. Throwing Response.Status.BAD_REQUEST");
                 throw new WebApplicationException(Response.Status.BAD_REQUEST);
             }
-
+            em.getTransaction().begin();
             // 3) Get desired seats to book from databsae, and check that they are all "not booked"
             String requestDateTime = bookingRequestDTO.getDate().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")); // Format datetime into appropriate string for request
             String requestLabelsListAsString = "('" + String.join("', '", bookingRequestDTO.getSeatLabels()) + "')"; // Create string representation of seat labels for query. Example: "('A1', 'B2', 'C3')" 
             String queryString = String.format("select s from Seat s where date = '%s' and label in %s", requestDateTime, requestLabelsListAsString);
-            TypedQuery<Seat> seatQuery = em.createQuery(queryString, Seat.class).setLockMode(LockModeType.PESSIMISTIC_WRITE);
+            TypedQuery<Seat> seatQuery = em.createQuery(queryString, Seat.class );
             List<Seat> desiredSeatsToBookFoundInDatabase = seatQuery.getResultList();
 
             for (Seat seat: desiredSeatsToBookFoundInDatabase) {
@@ -149,6 +149,7 @@ public class BookingResource {
                 seat.setBooked(true);
                 em.merge(seat);
                 LOGGER.info(String.format("Updated seat with id '%d' in database; it now has its isBooked field assigned true!", seat.getId()));
+
             }
 
             Set<Seat> desiredSeatsToBookFoundInDatabaseSet = new HashSet<>(desiredSeatsToBookFoundInDatabase); // Booking stores a set of seats, which makes sense - and we just got it out of the database as a list. This could be problematic.
@@ -218,7 +219,7 @@ public class BookingResource {
             em.getTransaction().begin();
 
             // 2) Get the booking information from the database 
-            Booking aBooking = em.find(Booking.class, id); // note: this has been lazily fetched (we don't want to actually go to the effort of getting the seat objects until we have authorised the user)
+            Booking aBooking = em.find(Booking.class, id, LockModeType.PESSIMISTIC_READ); // note: this has been lazily fetched (we don't want to actually go to the effort of getting the seat objects until we have authorised the user)
 
             if (aBooking == null) {
                 LOGGER.info("Booking does not exist with this id");
@@ -279,7 +280,7 @@ public class BookingResource {
 
             // 2) Get all bookings done by that user
             String queryString = String.format("select b from Booking b where b.user = '%d'", authenticatedUser.getId());
-            TypedQuery<Booking> bookingQuery = em.createQuery(queryString, Booking.class).setLockMode(LockModeType.PESSIMISTIC_WRITE);
+            TypedQuery<Booking> bookingQuery = em.createQuery(queryString, Booking.class);
             List<Booking> bookings = bookingQuery.getResultList();
 
             // 3) Convert to a list of BookingDTOs
